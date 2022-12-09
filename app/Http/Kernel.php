c5d9577f295faf2dd7df7cef3789628f030da36a
nguyen van hau <nguyenvanhau.soft@gmail.com>
2022-12-09 10:36:51 +0700

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-\GateGem\Core\Http\Middleware\CoreMiddleware::class,
+            \GateGem\Core\Http\Middleware\CoreMiddleware::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
